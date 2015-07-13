aac676e6194efcc98bd3b8e497c4f222cab3f5be
Eduardo Amendola <eduardoamendola@gmail.com>
2015-07-13 21:13:17 +0100

    Adding solution for task 01 from StudyTasks

---
+++ StudyTasks/01/php-hello-world/index.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
+	echo '<p>Hello World!</p>';
+	echo 'version 4';
+	echo '<P><P><A HREF="info.php">Link to phpinfo()</A>'
+ ?> 
+ </body>
+</html>		
